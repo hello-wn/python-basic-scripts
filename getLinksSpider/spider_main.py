34ca91d8f4ef40e4b746940544b5e0f3b5e8a1eb
hello-wn <hello_wn@yeah.net>
2018-04-22 18:43:57 +0800

    use beautifulsoup to craw urls contents from baidubaike

--- getLinksSpider/spider_main.py
+++ getLinksSpider/spider_main.py
@@ -20,7 +20,7 @@ class SpiderMain(object):
             self.urls.add_new_urls(new_urls)
             self.outputer.collect_data(new_data)
             count = count + 1
-            if count == 10:
+            if count == 100:
                break
          except:
             print('craw failed: %s' % (new_url))
